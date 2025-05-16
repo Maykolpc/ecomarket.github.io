b30dd36d76f7729118aa4a2e6c7ea787742ac7f8
Maykolpc <pcmaykel@gmail.com>
2025-05-16 17:13:16 -0500

    Delete scripts directory

--- scripts/app.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.add-to-cart').forEach(button => {
-    button.addEventListener('click', () => {
-        const productName = button.parentElement.querySelector('h3').textContent;
-        alert(`Añadido al carrito: ${productName}`);
-    });
-});
